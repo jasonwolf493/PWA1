e75d4be37137c397c3f5fb8d58dd7b35e6a161ca
jasonwolf493 <jasonwolf493@gmail.com>
2014-01-22 23:57:47 -0500

    Added alert for more specific troubleshooting

--- Conditionals_Industry/industry.js
+++ Conditionals_Industry/industry.js
@@ -22,4 +22,26 @@ alert("Nothing appears to be missing \nYou should still double check all of your
     console.log("Java Script found: " + javaScript);
     console.log("image folder found: " + imageFolder);
 
-}
+};
+
+
+if(indexPage != true || cascadingStyleSheet != true || javaScript != true || imageFolder != true){
+    if(indexPage === false){
+        alert("We couldn't find your index page.");
+    }
+    if(cascadingStyleSheet === false){
+        alert("Your CSS wasn't found.");
+    }
+    if(javaScript === false){
+        alert("Your Java Script couldn't be located.");
+    }
+    if(imageFolder === false){
+        alert("Your image folder wasn't found.")
+
+    }
+
+
+}else{
+
+    alert("The validation of all your files has been completed \nDouble check your files and run this again to be sure everything is where it should be.");
+};
