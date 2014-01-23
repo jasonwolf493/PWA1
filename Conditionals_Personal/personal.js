ccb3d838bf5de0693074866bacc21232da25e7f2
jasonwolf493 <jasonwolf493@gmail.com>
2014-01-23 02:14:28 -0500

    main info

--- Conditionals_Personal/personal.js
+++ Conditionals_Personal/personal.js
@@ -15,9 +15,38 @@ var day = 24;
 
 if(day >= 1){
    var work = prompt("How many hours a day do you work?\nYou have " + day + " hours available.");
+    var day = day - work;
+
     if(day >= 1){
-        var school = prompt("How many hours a day do you work?\nYou have " + day + " hours available.");
-    }
+        var school = prompt("How many hours a day do you work on school?\nYou have " + day + " hours available.");
+        var day = day - school;
+    }else{alert("You have no more time left in the day.");};
+
+    if(day >= 1){
+        var breakfast = prompt("How many hours a day do you work on breakfast? If you have breakfast at work enter 0.\nYou have " + day + " hours available.");
+        var day = day - breakfast;
+    }else{alert("You have no more time left in the day.");};
+
+    if(day >= 1){
+        var lunch = prompt("How many hours a day do take for lunch? If you have lunch at work enter 0.\nYou have " + day + " hours available.");
+        var day = day - lunch;
+    }else{alert("You have no more time left in the day.");};
+
+    if(day >= 1){
+        var dinner = prompt("How many hours a day do you take for dinner? If you have lunch at work enter 0.\nYou have " + day + " hours available.");
+        var day = day - dinner;
+    }else{alert("You have no more time left in the day.");};
+
+    if(day >= 1){
+        var travel = prompt("How many hours a day do you spend travelling?\nYou have " + day + " hours available.");
+        var day = day - travel;
+    }else{alert("You have no more time left in the day.");};
+
+    if(day >= 1){
+        var sleep = prompt("How many hours a day do you sleep?\nYou have " + day + " hours available.");
+        var day = day - sleep;
+    }else{alert("You have no more time left in the day.");};
+
 
 }else{
     alert("You have no more time left in the day.");
