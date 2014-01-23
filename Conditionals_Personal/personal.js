282ab814c43e35bee8a8de65eab2953a64ebfd75
jasonwolf493 <jasonwolf493@gmail.com>
2014-01-23 02:38:04 -0500

    double check project req.

--- Conditionals_Personal/personal.js
+++ Conditionals_Personal/personal.js
@@ -3,7 +3,7 @@
  */
 
 // Freetime calculator
-    // hours spent on activities
+    // declaring all of the vars
 var work = 8;
 var school = 8;
 var breakfast = 8;
@@ -14,6 +14,8 @@ var sleep = 8;
 var other = 8;
 var day = 24;
 
+
+//nested if statements prompting the user for the amount of hours they spend on each activity
 if(day >= 1){
    var work = prompt("How many hours a day do you work?\nYou have " + day + " hours available.");
     var day = day - work;
@@ -51,13 +53,17 @@ if(day >= 1){
     if(day >= 1){
         var other = prompt("How many hours a day do you spend on things that were not listed?\nYou have " + day + " hours available.");
         var day = day - other;
-    }else{alert("You have no more time left in the day.");};
+    }else{alert("You have no more time left in the day.");}
 
+
+// If there are no more hours left in the day it will tell the user they have used all of their aval. hours
 }else{
     alert("You have no more time left in the day.");
 
-};
+}
 
+
+// Displays the final results for all of the inputs.
 alert("You have finished entering your data. Please see the results below.\nYou spend " + work + " hours a day at work." + "\nYou spend " + school + " hours a day at school." + "\nYou spend " + breakfast + " hours a day on breakfast." + "\nYou spend " + lunch + " hours a day on lunch." + "\nYou spend " + dinner + " hours a day on dinner." + "\nYou spend " + travel + " hours a day traveling." + "\nYou spend " + sleep + " hours a day sleeping." + "\nYou spend " + other + " hours a day on unlisted activities." + "\nYou have a total of " + day + " unused hours a day.");
 
 
