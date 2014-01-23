435a788df9c18968d0583b85e708937981a800f6
jasonwolf493 <jasonwolf493@gmail.com>
2014-01-22 23:36:57 -0500

    testing missing file part of code

--- Conditionals_Industry/industry.js
+++ Conditionals_Industry/industry.js
@@ -4,7 +4,7 @@
 /* Website Build validator.... makes sure there are no files left out of a final web build */
 var indexPage = true;
 var cascadingStyleSheet = true;
-var javaScript = true;
+var javaScript = false;
 var imageFolder = true;
 
 if(indexPage && cascadingStyleSheet && javaScript && imageFolder){
