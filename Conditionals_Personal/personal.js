6dbea82e6c279bfa595cc1b08e756e7f56328f6c
jasonwolf493 <jasonwolf493@gmail.com>
2014-01-23 01:21:57 -0500

    first steps

---
+++ Conditionals_Personal/personal.js
@@ -0,0 +1,49 @@
+/**
+ * Created by jasonwolf on 1/16/14.
+ */
+
+// Freetime calculator
+    // hours spent on activities
+var work = 8;
+var school = 8;
+var breakfast = 8;
+var lunch = 8;
+var dinner = 8;
+var travel = 8;
+var sleep = 8;
+var day = 24;
+
+if(day >= 1){
+   var work = prompt("How many hours a day do you work?\nYou have " + day + " hours available.");
+    if(day >= 1){
+        var school = prompt("How many hours a day do you work?\nYou have " + day + " hours available.");
+    }
+
+}else{
+    alert("You have no more time left in the day.");
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
