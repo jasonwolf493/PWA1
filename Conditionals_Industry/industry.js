f0c3e9200c32843cdaa4c2eb95f2c4f078baa742
jasonwolf493 <jasonwolf493@gmail.com>
2014-01-22 23:24:21 -0500

    working && if statement

--- Conditionals_Industry/industry.js
+++ Conditionals_Industry/industry.js
@@ -4,13 +4,14 @@
 /* Website Build validator.... makes sure there are no files left out of a final web build */
 var indexPage = true;
 var cascadingStyleSheet = true;
-var javaScript = false;
+var javaScript = true;
 var imageFolder = true;
 
 if(indexPage && cascadingStyleSheet && javaScript && imageFolder){
 
-
+console.log("all is working");
+console.log("Index page found: " + indexPage);
 }else{
-
+console.log("not working");
 
 }
