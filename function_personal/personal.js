316ea30c8fec837713a71dc5d62d7c15fc697d17
jasonwolf493 <Jason@Jasons-MacBook-Pro.local>
2014-01-30 02:56:53 -0500

    added a loop for multiple reminders for each time of day!

--- function_personal/personal.js
+++ function_personal/personal.js
@@ -8,22 +8,23 @@ function reminder(date, time, plans){
 }
 
 
-
-
+var repeat = "yes"
+while(repeat == "yes"){
 //morning plans
-var morning = prompt("do you have plans for the morning?");
-if(morning == "yes"){
-    var c = prompt("What are your plans for the morning?");            //What the plan for the morning is
-    var setReminder = prompt("would you like to set a reminder for the morning event?\nyes or no");
-    if(setReminder == "yes"){
-        var a = prompt("What will the date be for your event?");      //date of event
-        var b = prompt("What will the time for the event be?");       //Time of event
-        reminder(a, b, c);
-    }else{
-        setReminder = false;                        // resets setReminder to false
-    }
-}else{}
-
+    var morning = prompt("do you have plans for the morning?");
+    if(morning == "yes"){
+        var c = prompt("What are your plans for the morning?");            //What the plan for the morning is
+        var setReminder = prompt("would you like to set a reminder for the morning event?\nyes or no");
+        if(setReminder == "yes"){
+            var a = prompt("What will the date be for your event?");      //date of event
+            var b = prompt("What will the time for the event be?");       //Time of event
+            reminder(a, b, c);
+            var repeat = prompt("would you like to add another event?");
+        }else{
+            setReminder = false;                        // resets setReminder to false
+        }
+    }else{}
+}
 
 //Afternoon plans
 var afternoon = prompt("do you have plans for the afternoon?");
