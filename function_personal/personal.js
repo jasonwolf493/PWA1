f710d210d7521e94ce7b19f6c9a3989f9f24c745
jasonwolf493 <Jason@Jasons-MacBook-Pro.local>
2014-01-30 01:59:59 -0500

    added some notes

--- function_personal/personal.js
+++ function_personal/personal.js
@@ -1,17 +1,18 @@
 /**
  * Created by jasonwolf on 1/16/14.
  */
+//a small function to remind the user of a date, time and the plans they have that day.
 function reminder(date, time, plans){
     console.log("You have a reminder set for "+date+" At "+time+" for your event of... "+plans);
     alert("You have a reminder set for "+date+" at "+time+" for your event of... "+plans);
 }
 
 
+
+
 //morning plans
 var morning = prompt("do you have plans for the morning?");
-
 if(morning == "yes"){
-
     var c = prompt("What are your plans for the morning?");
     var setReminder = prompt("would you like to set a reminder for the morning event?\nyes or no");
     if(setReminder == "yes"){
@@ -26,9 +27,7 @@ if(morning == "yes"){
 
 //Afternoon plans
 var afternoon = prompt("do you have plans for the afternoon?");
-
 if(afternoon == "yes"){
-
     var c = prompt("What are your plans for the afternoon?");
     var setReminder = prompt("would you like to set a reminder for the afternoon event?\nyes or no");
     if(setReminder == "yes"){
