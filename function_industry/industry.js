a483e8bd1c652227d2f63853a385c7e1d787cc02
jasonwolf493 <Jason@Jasons-MacBook-Pro.local>
2014-01-30 21:58:57 -0500

    added an update checker.

--- function_industry/industry.js
+++ function_industry/industry.js
@@ -16,4 +16,18 @@ var uploaded = 0;
 
     }
 }
-backup(files);
+
+function updateChecker(files){
+    files = files / 2 + 2;
+    var update = prompt(files +" Files will be updated would you like to continue?");
+    var updated = 0;
+    if(update=="yes"){
+        while(files >= 1){
+            updated++;
+            files--;
+            console.log(updated+" files were updated on your system "+files+" Remaining...");
+
+        }
+    }
+}
+updateChecker(files);
