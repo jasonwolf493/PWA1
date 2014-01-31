91021cfbbe3841023c75e01a32cad1b882c95ec1
jasonwolf493 <Jason@Jasons-MacBook-Pro.local>
2014-01-30 21:35:04 -0500

    started backup function

--- function_industry/industry.js
+++ function_industry/industry.js
@@ -1,3 +1,18 @@
 /**
  * Created by jasonwolf on 1/16/14.
  */
+//update checker and backup.
+
+
+
+//update checker
+files = 10;
+function backup(files){
+var uploaded = 0;
+    while(files >= 1){
+    uploaded++;
+    console.log(uploaded+" files were uploaded to the server "+files+" Remaining");
+        files--;
+    }
+}
+backup(files);
