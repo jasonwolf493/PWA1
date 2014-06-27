b8f80ea7a634e0471751eb62f4c5258e9a349060
jasonwolf493 <Jason@Jasons-MacBook-Pro.local>
2014-06-27 14:44:22 -0400

    working min.'s

--- WK4/goal7_assign_obj_to_prototypes/js/main.js
+++ WK4/goal7_assign_obj_to_prototypes/js/main.js
@@ -4,3 +4,42 @@
  * Goal: Goal7
  */
 
+(function(){
+    //all vars
+    var numOfPeople =3;
+    var people = [];
+    var names = ["Jason","Ange","Bob","John","Jane"];
+    var interval;
+
+    //make the loop for person
+    for(var i = 0; i<numOfPeople;i++){
+
+        //make a number between 1-5
+        var personIndex = Math.floor(Math.random()*names.length);
+
+        //New person
+        var person = new Person(names[personIndex],i+1);
+
+        populateHTML(person.name,"r"+(i+1)+"c1");
+        populateHTML(person.job,"r"+(i+1)+"c2");
+
+        people.push(person);
+        names.splice(personIndex,1);
+    }
+
+    clearInterval(interval);
+
+    //set interval
+    interval=setInterval(runUpdate,1000 / 30);
+
+    function populateHTML(data,field){
+        var id=document.getElementById(field);
+        id.innerHTML = data;
+    }
+
+    function runUpdate(){
+        people.forEach(function(element){
+            element.update();
+        });
+    }
+})();
